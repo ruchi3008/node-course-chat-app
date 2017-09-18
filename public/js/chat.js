2ac00274d6d7678873d354be4e15c51d44ea3030
Ruchi <ruchi@localhost.localdomain>
2017-09-18 16:55:21 -0700

    send messages to only people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -78,7 +78,6 @@ jQuery('#message-form').on('submit',function(e){
 e.preventDefault();
 var messageTextBox = jQuery('[name=message]')
 socket.emit('createMessage',{
-  from : 'Admin',
   text : messageTextBox.val()
 },function(){
   messageTextBox.val('');
